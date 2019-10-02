e3ec5c6d1caa58273ed88cb9b828b6b01b013c4a
jnparamitha <jnparamitha@gmail.com>
2019-10-02 16:37:19 +0700

    Listener Fragment

--- app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
@@ -8,27 +8,38 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import id.ac.polinema.idealbodyweight.fragments.AboutFragment;
+import id.ac.polinema.idealbodyweight.fragments.BrocaIndexFragment;
+import id.ac.polinema.idealbodyweight.fragments.MenuFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+		MenuFragment.OnFragmentInteractionListener,
+        BrocaIndexFragment.OnFragmentInteractionListener {
 
-	// Deklarasikan atribut Fragment di sini
-	private AboutFragment aboutFragment;
+		// Deklarasikan atribut Fragment di sini
+		private AboutFragment aboutFragment;
+		private MenuFragment menuFragment;
+        private BrocaIndexFragment brocaIndexFragment;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+		@Override
+		protected void onCreate (Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		aboutFragment = AboutFragment.newInstance("Jihan Nabilla Paramita");
+		    menuFragment = new MenuFragment();
+			getSupportFragmentManager().beginTransaction()
+					.replace(R.id.fragment_container, menuFragment)
+					.commit();
+			brocaIndexFragment = new BrocaIndexFragment();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+		@Override
+		public boolean onCreateOptionsMenu (Menu menu){
 		getMenuInflater().inflate(R.menu.menu, menu);
-		return  true;
+		return true;
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+		@Override
+		public boolean onOptionsItemSelected (@NonNull MenuItem item){
 		// TODO: Tambahkan penanganan menu di sini
 		if (item.getItemId() == R.id.menu_about) {
 			getSupportFragmentManager().beginTransaction()
@@ -38,4 +49,22 @@ public class MainActivity extends AppCompatActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+	@Override
+	public void onBrocaIndexButtonClicked()
+    {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, brocaIndexFragment)
+                .commit();
+	}
+
+	@Override
+	public void onBodyMassIndexButtonClicked() {
+
+	}
+
+    @Override
+    public void onCalculateBrocaIndexClicked(float index) {
+
+    }
 }
